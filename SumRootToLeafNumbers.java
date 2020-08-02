2606a9a21b14892748e4721ff89dcb036f7307ba
Arpit Sharma <arpit317@gmail.com>
2020-08-02 16:00:12 -0700

    Update SumRootToLeafNumbers.java

--- SumRootToLeafNumbers.java
+++ SumRootToLeafNumbers.java
@@ -68,3 +68,26 @@ class Solution {
     }
 }
 
+
+//Recursion -- global variable
+class Solution {
+    int result = 0;
+    public int sumNumbers(TreeNode root) {
+        if(root == null) return 0;
+        helper(root, 0);
+        return result;
+    }
+    private void helper(TreeNode root, int currSum){
+        if(root == null) return;
+        
+        currSum = currSum * 10 + root.val;
+
+        if(root.left == null && root.right == null) {
+            result += currSum;
+            return;
+        }
+        
+        helper(root.left, currSum);
+        helper(root.right, currSum);
+    }
+}
